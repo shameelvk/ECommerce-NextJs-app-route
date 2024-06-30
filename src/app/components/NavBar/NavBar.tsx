9b78aeee6e2688cedebc4bf14c19b7510ecd48d6
shameelvk <shameel3311@gmail.com>
2024-06-30 14:07:30 +0530

    fix:deployment error with navbar

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation';
-import './Navbar.css';
+import './NavBar.css';
 import { ProductServices } from '@/app/services/productServices';
 import { useCartCount } from '@/app/contexts/CartCoutContext';
 
